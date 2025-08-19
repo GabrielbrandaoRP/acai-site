5b8ee0f9b4895583741c5e027539b6a75a0c78e4
GabrielbrandaoRP <gabrielhand15@gmail.com>
2025-08-19 20:09:45 -0300

    Ajuste da logo na NavBar e registro das midias sem fundo e em SVG

--- src/app/_components/NavBar.tsx
+++ src/app/_components/NavBar.tsx
@@ -1,11 +1,16 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import logo_sem_circulo from "@/assets/logo_sem_circulo.svg"
 
 const NavBar = () => {
   return (
     <>
-    <header className="flex justify-between items-center px-5 py-2 bg-[#9400D3] h-24">
-        <Link href="/"><h1 className="font-bold text-2xl">Logo</h1></Link>
+      <header className="flex justify-between items-center px-5 py-4 bg-[#9400D3] h-24">
+        <div className=''>
+        <Link href="/"><Image src={logo_sem_circulo} alt='Batidinha de açaí' quality={100} width={230} height={100}/></Link>
+
+        </div>
         <nav>
           <ul className=" flex gap-10 list-style-type:none text-white font-medium text-xl">
             <li><Link href="/">Quem somos</Link></li>
